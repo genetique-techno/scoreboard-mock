a5d84b1427e0cda10fe0af75946ec92744fa5395
Jmeas <jellyes2@gmail.com>
2014-01-28 10:55:36 -0500

    Initial push

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
 mock-endpoint
 =============
 
-A mockjax endpoint
+This is a mock endpoint that can be used to build a dynamic league score board.
+
+### Installation
+
+- Clone this repository  
+  `git clone https://github.com/mobilereactor/mock-endpoint.git`
+- Install `jQuery` into your project. Download [here](https://github.com/jquery/jquery)  
+  `<script src='path/to/jquery.js'></script>`
+- Install `Mockjax` into your project after `jQuery`. Download [here](https://github.com/appendto/jquery-mockjax)  
+  `<script src='path/to/jquery.mockjax.js'></script>`
+- Install `server.js` into your project (included in this repo)  
+  `<script src='path/to/server.js'></script>`
+
+### Usage
+
+Any request sent to the URL `/schedule/week/1/` via `jQuery` Ajax methods will be intercepted by Mockjax. Note that all Backbone server calls (i.e.; `model.fetch`) are powered by jQuery's Ajax method.
+
+### Response
+
+The response is an object with a single property: `schedule`. The value of this property is an array.
+
+Each entry in the array is an object with the following properties:
+
+- `AScore` - The away team's score
+- `HScore` - The home team's score
+- `Game_Date` - Self-explanatory, I think
+- `Game_Time` - See above
+- `Location` - The stadium the game is taking place in
+- `Schedule_id` - A unique identifier for this game
+- `SeasonType` - A string representing the type of game
+- `Status` - The status of the game
+- `AwayTeam` - An object representing the away team
+- `HomeTeam` - An object representing the home team
+
+I encourage you to look into the away and home teams to figure out their structure
---
+++ server.js
@@ -0,0 +1,52 @@
+/*
+ *
+ * Configures a mock server at the URL:
+ * `schedule/week/1`
+ *
+ */
+
+(function() {
+
+  $.mockjax({
+
+    // The last slash is optional
+    url: /^\/schedule\/week\/1\/?$/,
+    responseTime: 500,
+    response: function( settings ) {
+      baseSchedule.forEach(function(value, index, array) {
+        value.AScore = randomInt( 0, 40 );
+        value.HScore = randomInt( 0, 40 );
+      });
+      var responseText = {
+        schedule: baseSchedule
+      };
+      this.responseText = responseText;
+    }
+  });
+
+  var randomInt = function( min, max ) {
+    return Math.floor( Math.random() * (max-min+1) + min );
+  };
+
+  // This object is (destructively) changed with each mock call to the server
+  var baseSchedule = [
+    {"Schedule_id":"4175","Game_Date":"09/05/2013","Game_Time":"08:30 PM","Status":"FINAL","Location":"Sports Authority Field at Mile High Stadium","AScore":"27","HScore":"49","AwayTeam":{"_":"Baltimore","AwayTeamid":"065"},"HomeTeam":{"_":"Denver","HomeTeamid":"067"},"SeasonType":"Regular"},
+    {"Schedule_id":"4176","Game_Date":"09/08/2013","Game_Time":"01:00 PM","Status":"FINAL","Location":"Ralph Wilson Stadium","AScore":"23","HScore":"21","AwayTeam":{"_":"New England","AwayTeamid":"077"},"HomeTeam":{"_":"Buffalo","HomeTeamid":"062"},"SeasonType":"Regular"},
+    {"Schedule_id":"4177","Game_Date":"09/08/2013","Game_Time":"01:00 PM","Status":"FINAL","Location":"Heinz Field","AScore":"16","HScore":"9","AwayTeam":{"_":"Tennessee","AwayTeamid":"070"},"HomeTeam":{"_":"Pittsburgh","HomeTeamid":"082"},"SeasonType":"Regular"},
+    {"Schedule_id":"4179","Game_Date":"09/08/2013","Game_Time":"01:00 PM","Status":"FINAL","Location":"MetLife Stadium","AScore":"17","HScore":"18","AwayTeam":{"_":"Tampa Bay","AwayTeamid":"087"},"HomeTeam":{"_":"NY Jets","HomeTeamid":"080"},"SeasonType":"Regular"},
+    {"Schedule_id":"4180","Game_Date":"09/08/2013","Game_Time":"01:00 PM","Status":"FINAL","Location":"EverBank Field","AScore":"28","HScore":"2","AwayTeam":{"_":"Kansas City","AwayTeamid":"072"},"HomeTeam":{"_":"Jacksonville","HomeTeamid":"090"},"SeasonType":"Regular"},
+    {"Schedule_id":"4182","Game_Date":"09/08/2013","Game_Time":"01:00 PM","Status":"FINAL","Location":"Soldier Field","AScore":"21","HScore":"24","AwayTeam":{"_":"Cincinnati","AwayTeamid":"064"},"HomeTeam":{"_":"Chicago","HomeTeamid":"063"},"SeasonType":"Regular"},
+    {"Schedule_id":"4183","Game_Date":"09/08/2013","Game_Time":"01:00 PM","Status":"FINAL","Location":"FirstEnergy Stadium","AScore":"23","HScore":"10","AwayTeam":{"_":"Miami","AwayTeamid":"075"},"HomeTeam":{"_":"Cleveland","HomeTeamid":"120"},"SeasonType":"Regular"},
+    {"Schedule_id":"4185","Game_Date":"09/08/2013","Game_Time":"01:00 PM","Status":"FINAL","Location":"Lucas Oil Stadium","AScore":"17","HScore":"21","AwayTeam":{"_":"Oakland","AwayTeamid":"073"},"HomeTeam":{"_":"Indianapolis","HomeTeamid":"071"},"SeasonType":"Regular"},
+    {"Schedule_id":"4191","Game_Date":"09/09/2013","Game_Time":"10:20 PM","Status":"FINAL","Location":"Qualcomm Stadium","AScore":"31","HScore":"28","AwayTeam":{"_":"Houston","AwayTeamid":"151"},"HomeTeam":{"_":"San Diego","HomeTeamid":"083"},"SeasonType":"Regular"},
+    {"Schedule_id":"4178","Game_Date":"09/08/2013","Game_Time":"01:00 PM","Status":"FINAL","Location":"Mercedes-Benz Superdome","AScore":"17","HScore":"23","AwayTeam":{"_":"Atlanta","AwayTeamid":"061"},"HomeTeam":{"_":"New Orleans","HomeTeamid":"078"},"SeasonType":"Regular"},
+    {"Schedule_id":"4181","Game_Date":"09/08/2013","Game_Time":"01:00 PM","Status":"FINAL","Location":"Bank of America Stadium","AScore":"12","HScore":"7","AwayTeam":{"_":"Seattle","AwayTeamid":"085"},"HomeTeam":{"_":"Carolina","HomeTeamid":"089"},"SeasonType":"Regular"},
+    {"Schedule_id":"4184","Game_Date":"09/08/2013","Game_Time":"01:00 PM","Status":"FINAL","Location":"Ford Field","AScore":"24","HScore":"34","AwayTeam":{"_":"Minnesota","AwayTeamid":"076"},"HomeTeam":{"_":"Detroit","HomeTeamid":"068"},"SeasonType":"Regular"},
+    {"Schedule_id":"4186","Game_Date":"09/08/2013","Game_Time":"04:25 PM","Status":"FINAL","Location":"Candlestick Park","AScore":"28","HScore":"34","AwayTeam":{"_":"Green Bay","AwayTeamid":"069"},"HomeTeam":{"_":"San Francisco","HomeTeamid":"084"},"SeasonType":"Regular"},
+    {"Schedule_id":"4187","Game_Date":"09/08/2013","Game_Time":"04:25 PM","Status":"FINAL","Location":"Edward Jones Dome","AScore":"24","HScore":"27","AwayTeam":{"_":"Arizona","AwayTeamid":"086"},"HomeTeam":{"_":"St. Louis","HomeTeamid":"074"},"SeasonType":"Regular"},
+    {"Schedule_id":"4188","Game_Date":"09/08/2013","Game_Time":"08:30 PM","Status":"FINAL","Location":"AT&T Stadium","AScore":"31","HScore":"36","AwayTeam":{"_":"NY Giants","AwayTeamid":"079"},"HomeTeam":{"_":"Dallas","HomeTeamid":"066"},"SeasonType":"Regular"},
+    {"Schedule_id":"4189","Game_Date":"09/09/2013","Game_Time":"06:55 PM","Status":"FINAL","Location":"FedEx Field","AScore":"33","HScore":"27","AwayTeam":{"_":"Philadelphia","AwayTeamid":"081"},"HomeTeam":{"_":"Washington","HomeTeamid":"088"},"SeasonType":"Regular"}
+  ];
+
+})();
+
