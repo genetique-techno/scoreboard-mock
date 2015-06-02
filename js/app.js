5731f753c2cfbfe85e494a9fb3bec192bcafa433
keiichi189 <aaron.todd@gmail.com>
2015-06-01 21:49:28 -0400

    updating with new patterns

--- js/app.js
+++ js/app.js
@@ -1,18 +1,8 @@
 
-function friendlyDate(date) {
-	// given input string 'MM/DD/YYYY', returns string 'Day, Month Date, Year'
-	var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-	var exp = /(\d+)\/(\d+)\/(\d+)/g;
-	var result = exp.exec(date);
-	var parsed = new Date(result[3], result[1], result[2]);
-
-	return pretty_date = day[parsed.getDay()] + ', ' + month[parsed.getMonth()] + ' ' + parsed.getDate() + ', ' + result[3];
-}
-
 var Game = Backbone.Model.extend({
 	// Creates constructor for Model
 	defaults: {
+		id: '',
 		Game_Date: '',
 		Game_Time: '',
 		Status: '',
@@ -27,48 +17,47 @@ var Games = Backbone.Collection.extend({
 	// Creates constructor for Collection
 	url: '/schedule/week/1',
 	model: Game,
-	//parse: function (response) {
-	//	console.log(response);
-	//	return response.items;
-	//}
-	
-
+	parse: function (data) {
+		console.log(data);
+		return data.map( function (item, idx) {
+			return {
+				Game_Date: this.friendlyDate(item.Game_Date),
+				Game_Time: item.Game_Time,
+				Status: item.Status,
+				HomeTeam: item.HomeTeam._,
+				HScore: item.HScore,
+				AScore: item.AScore,
+				AwayTeam: item.AwayTeam._
+			};
+		}, this);
+	},
+	friendlyDate: function (date) {
+		// given input string 'MM/DD/YYYY', returns string 'Day, Month Date, Year'
+		var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+		var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+		var exp = /(\d+)\/(\d+)\/(\d+)/g;
+		var result = exp.exec(date);
+		var parsed = new Date(result[3], result[1], result[2]);
+		return pretty_date = day[parsed.getDay()] + ', ' + month[parsed.getMonth()] + ' ' + parsed.getDate() + ', ' + result[3];
+	}
 });
 
 var View = Backbone.View.extend({
 	// Creates constructor for View
 	el: $('#container'),
 	initialize: function () {
+		var tmplText = $('#game-thing-tmpl').html();
+		this.tmpl = _.template(tmplText);		
 		this.listenTo(this.collection, 'sync change', this.render);
 		this.collection.fetch();
-		var tmplText = $('#game-thing-tmpl').html();
-		var tmpl = _.template(tmplText);
 	},
 	render: function () {
 		this.$el.empty();
-		for (var i = 0; i < this.collection.length; i++) {
-			/* var Game_Date = friendlyDate(this.collection.at(i).attributes.Game_Date);
-			var Game_Time = this.collection.at(i).attributes.Game_Time;
-			var Status = this.collection.at(i).attributes.Status;
-			var HomeTeam = this.collection.at(i).attributes.HomeTeam._;
-			var HScore = this.collection.at(i).attributes.HScore;
-			var AScore = this.collection.at(i).attributes.AScore;
-			var AwayTeam = this.collection.at(i).attributes.AwayTeam._;
-			this.tmpl
-			*/
+		this.collection.forEach( function (item) {
+			var r = this.tmpl(item.attributes);
+			this.$el.append(r);
+		}, this);
 
-			
-			var html = '<div class=\"thing_header\"><div class=\"date\">';
-			html += friendlyDate(this.collection.at(i).attributes.Game_Date) + ' at ';
-			html += this.collection.at(i).attributes.Game_Time + '</div><div class=\"status\">';
-			html += this.collection.at(i).attributes.Status + '</div></div><div class=\"thing\"><div class=\"hteam">';
-			html += this.collection.at(i).attributes.HomeTeam._ + '</div><div class=\"hscore\">';
-			html += this.collection.at(i).attributes.HScore + '</div><div class=\"ascore\">';
-			html += this.collection.at(i).attributes.AScore + '</div><div class=\"ateam\">';
-			html += this.collection.at(i).attributes.AwayTeam._ + '</div></div>';
-			this.$el.append(html);
-			
-		};
 		return this;  // allows for chained calls
 	}
 
@@ -76,8 +65,4 @@ var View = Backbone.View.extend({
 
 var games = new Games();  // instance of Games collection
 var view = new View({ collection: games });  // instance of View
-var fetch = function () {
-	// fetching function expression
-	games.fetch();
-};
-window.setInterval(fetch, 10000);  // fetches every 10 seconds
+window.setInterval(function () {view.collection.fetch()}, 10000);
